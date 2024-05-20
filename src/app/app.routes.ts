f1caff885a5346ecb44bdf5ab67c4d7f6362ba11
Luiggi <luiggiyantas@gmail.com>
2024-05-19 23:29:11 -0500

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -15,5 +15,10 @@ export const routes: Routes = [
     {
         path: 'view',
         component: ViewComponent
+    },
+    {
+        path: '',
+        redirectTo: 'codes',
+        pathMatch: 'full'
     }
 ];
